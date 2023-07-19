142d4ed4149c11665a082cc936fab6df5bbf9dd6
freeanser <bnn01615@gmail.com>
2023-07-19 17:15:03 +0800

    feat: add main.hbs

--- app.js
+++ app.js
@@ -2,24 +2,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')// 為了讀取到 req.body 的套件
-const methodOverride = require('method - override')
+const methodOverride = require('method-override')
 
 
 // 內部
 const app = express()
 const port = 3000
-const routes = require('./routes')
-
+// const routes = require('./routes')
+require('dotenv').config()
+require('./config/mongoose')
 
 // 引用
-app.use(express)
-app.use('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs') // 將視圖引擎設置為 Handlebars。這樣一來，當你使用 res.render 方法來呈現視圖時，Express 將使用 Handlebars 模板引擎來編譯視圖並返回給客戶端
-app.use(bodyParser)
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(routes)
+// app.use(routes)
 
-
+app.get('/', (req, res, next) => {
+  res.render('index')
+})
 
 app.listen(port, () => {
   console.log(`Serve is running on http://localhost:${port}`)
---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+} //當應用程式不是在生產環境中運行時（即 NODE_ENV 不等於 'production'），就會執行 require('dotenv').config()。這將引入 "dotenv" 套件並讀取 ".env" 檔案的內容，然後將其中的環境變數加載到應用程式的環境變數中。
+
+// 連線
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+}) // on 是事件監聽器
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
