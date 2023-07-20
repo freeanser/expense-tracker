25a19bd37eb1ec15aa3665e4065dee14b839f2b4
freeanser <bnn01615@gmail.com>
2023-07-20 18:16:21 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')// 為了讀取到 req.body 的套件
 const methodOverride = require('method-override')
-
+const moment = require('moment');
 
 // 內部
 const app = express()
@@ -13,15 +13,18 @@ require('dotenv').config()
 require('./config/mongoose')
 
 // 引用
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({
+  defaultLayout: 'main', extname: '.hbs', helpers: {
+    dateFormat: function (date, format) {
+      return moment(date).format(format);
+    }
+  }
+}))
 app.set('view engine', 'hbs') // 將視圖引擎設置為 Handlebars。這樣一來，當你使用 res.render 方法來呈現視圖時，Express 將使用 Handlebars 模板引擎來編譯視圖並返回給客戶端
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.get('/', (req, res, next) => {
-  res.render('index')
-})
 
 app.listen(port, () => {
   console.log(`Serve is running on http://localhost:${port}`)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,8 +4,16 @@ const router = express.Router()
 
 // add models
 const Category = require('../../models/category')
+const Record = require('../../models/record')
 
 // show front page
-router.use('/', (req, res) => {
-  res.render('index')
+router.get('/', (req, res) => {
+
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(error => console.log(error))
 })
+
+// routes 要記得導出去！！！
+module.exports = router
