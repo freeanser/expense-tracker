2ccb3d9108fe81239852b44bc20a7bb54c373968
freeanser <bnn01615@gmail.com>
2023-07-22 23:25:25 +0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,25 @@
+// add database
+const mongoose = require('mongoose')
+
+// add schema
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
