0454d6a29e90c24630a988f954578443bf7472da
freeanser <bnn01615@gmail.com>
2023-07-22 23:46:53 +0800

    feat: add user registration function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,6 +5,7 @@ const router = express.Router()
 // add models
 const Category = require('../../models/category')
 const Record = require('../../models/record')
+const User = require('../../models/user')
 
 // show login page
 router.get('/login', (req, res) => {
@@ -18,6 +19,26 @@ router.get('/register', (req, res) => {
 
 // submit login 
 
+
+// submit register
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  // 找找看～ if user already had an account
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', {
+          name, email, password, confirmPassword
+        })
+      } else {
+        // create new account
+        return User.create({ name, email, password })
+          .then(() => res.redirect('/'))
+      }
+    })
+    .catch(error => console.log(error))
+})
 
 // routes 要記得導出去！！！
 module.exports = router
