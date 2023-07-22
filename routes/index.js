a40e01a19c09fe065d2bf45a2b8f6e66970692bf
freeanser <bnn01615@gmail.com>
2023-07-22 18:55:25 +0800

    feat: add login page

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 // add router modules
 const home = require('./modules/home')
 const records = require('./modules/records')
+const users = require('./modules/users')
 
 // add routes
 router.use('/records', records)
+router.use('/users', users)
 router.use('/', home)
 
 module.exports = router
---
+++ routes/modules/users.js
@@ -0,0 +1,18 @@
+// add express and router
+const express = require('express')
+const router = express.Router()
+
+// add models
+const Category = require('../../models/category')
+const Record = require('../../models/record')
+
+// show login page
+router.get('/login', (req, res) => {
+  return res.render('login')
+})
+
+// submit login 
+
+
+// routes 要記得導出去！！！
+module.exports = router
