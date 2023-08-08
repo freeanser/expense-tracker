af1c542972f2f6309cb13e8605f27ab450f7c567
freeanser <bnn01615@gmail.com>
2023-08-08 23:08:05 +0800

    modify: fix login page and add user.json

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = app => {
   // 這些 app.use 等初始化動作，以前都習慣寫在 app.js 裡，這裡由於建立了專門的 passport 設定檔，我們就拉到設定檔裡來統一管理
@@ -10,23 +11,26 @@ module.exports = app => {
   app.use(passport.session()) // 初始化 Passport 的 Session 功能，這允許 Passport 保持用戶登入的狀態，並管理用戶會話
   // 設定本地登入策略
   //  { usernameField: 'email' }把驗證項目改成 email
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     // done 是一個回調函式，用於指示身份驗證的結果
     User.findOne({ email })
       .then(user => {
         if (!user) {
           // done(null驗證過程沒有錯誤, false找不到使用者, info)
-          return done(null, false, { message: 'That email is not registered!' })
+          return done(null, false, req.flash('warning_msg', 'That email is not registered!'))
         }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password incorrect.' })
-        }
-        // done(null, user找到使用者，將使用者物件傳給 done)
-        return done(null, user)
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, req.flash('warning_msg', 'Email 或 Password 不正確。'))
+          }
+          // done(null, user找到使用者，將使用者物件傳給 done)
+          return done(null, user)
+        })
       })
       // done(err驗證過程出現錯誤, false找不到使用者)
       .catch(err => done(err, false))
   }))
+
   // 設定序列化與反序列化
   // passport.serializeUser將使用者登入時的使用者物件，轉換成識別資訊並儲存在 Session 中
 
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,30 +1,37 @@
+// add record seeds
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+//當應用程式不是在生產環境中運行時（即 NODE_ENV 不等於 'production'），就會執行 require('dotenv').config()。這將引入 "dotenv" 套件並讀取 ".env" 檔案的內容，然後將其中的環境變數加載到應用程式的環境變數中。
+
 // add database mongoose
 const mongoose = require('mongoose')
 const Record = require('../record')
+const User = require('../user')
+const Category = require('../category')
 
-// env
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-} //當應用程式不是在生產環境中運行時（即 NODE_ENV 不等於 'production'），就會執行 require('dotenv').config()。這將引入 "dotenv" 套件並讀取 ".env" 檔案的內容，然後將其中的環境變數加載到應用程式的環境變數中。
+const recordList = require('../../record.json').records
+const userList = require('../../user.json')
 
-// seeder
-const SEED_USER = {
-  name: '午餐',
-  date: '2019/4/23',
-  amount: 60,
-}
-
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-}) // on 是事件監聽器
+const db = require('../../config/mongoose')
+const categories = []
 
 // add data
 db.once('open', () => {
-  console.log('mongodb connected')
-  Record.create(SEED_USER)
+  Promise.all(
+    // 幫種子資料加鹽
+    userList.map(seedUser => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(seedUser.password, salt))
+        .then(hash => {
+          return User.create({
+            name: seedUser.name,
+            email: seedUser.email,
+            password: hash
+          })
+            .catch(err => console.log(err))
+        })
+    }))
 })
